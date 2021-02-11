494e1061312c71508db3bb2666979eae07f797ff
saerx <saerlaith.robyn@gmail.com>
2021-02-11 12:19:43 +0000

    added models

---
+++ src/main/java/com/example/codeclan/courseBooking/models/Course.java
@@ -0,0 +1,85 @@
+package com.example.codeclan.courseBooking.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CollectionId;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String town;
+
+    @Column
+    private int starRating;
+
+    @OneToMany(mappedBy = "course")
+    @JsonIgnoreProperties({"courses"})
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int starRating) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
+}
